d76b78d3614508ba48dfa90c873d79f2588ca700
niththish <niththish21@gmail.com>
2022-08-29 23:39:30 +0530

    configuring auth middleware in /api/birthdays route

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const morgan = require('morgan')
 const birthdaysRouter = require('./routes/birthdays')
 const authRouter = require('./routes/auth')
 const databaseConnection = require('./config/database')
+const authenticate = require('./middleware/authenticate')
 require('dotenv').config()
 
 const app = express()
@@ -13,7 +14,7 @@ app.use(express.json())
 // routes
 app.get('/', (req, res) => { res.send('Hello World');  })
 app.use('/auth',authRouter)
-app.use('/api/birthdays',birthdaysRouter)
+app.use('/api/birthdays',authenticate,birthdaysRouter)
 
 
 // start server
