d535afb2aff3ebf6680069184d7d5b52fc7af62d
niththish <niththish21@gmail.com>
2022-08-29 22:40:32 +0530

    logic implementation for signup page

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,19 @@
+const userSchema = require('../models/users')
+
 const login = async(req,res,next) => {
     res.send("login page")
 }
 
 const signup = async(req,res,next) => {
-    res.send("signup")
+    const { username, password } = req.body;
+    console.log(username,password);
+    if(!username || !password) return next("provide username and password")
+    const user = await userSchema.findOne({username})
+    if(!user) {
+        const user = await userSchema.create(req.body)
+        return res.send("registered successfully")
+    }
+    return next("username already taken")
 }
 
 module.exports = { login, signup }
--- models/users.js
+++ models/users.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
 const userSchema = mongoose.Schema({
     username : {
@@ -10,6 +11,12 @@ const userSchema = mongoose.Schema({
         type : String,
         required : [true,'password must be provided']
     }
+})
+
+userSchema.pre('save',async function(next){
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password,salt)
+    next()
 })
 
 module.exports = mongoose.model('users',userSchema)
